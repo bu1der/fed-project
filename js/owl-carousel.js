43d52408118e022716c322c1583fe18ac4ec1287
Oleksiy Alex <oleksiyalex3@gmail.com>
2017-12-11 18:54:24 +0200

    Change theme

---
+++ js/owl-carousel.js
@@ -0,0 +1,29 @@
+$( document).ready(function() {
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+	stagePadding: 50,
+	center: true,
+    loop:true,
+    margin:52,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+});
