41b8fc7b0690f515df0df86a1400b9bb33c76ab8
Oleksiy Alex <oleksiyalex3@gmail.com>
2017-12-18 20:32:19 +0200

    Make log in with javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function check(form) {
+	if(form.userid.value == "fwd-06-5" && form.passid.value == "qwert-12")
+	{
+		window.open('https://bu1der.github.io/about.html')
+	}
+	else
+	{
+		alert("Нагадати пароль")
+	}
+}
