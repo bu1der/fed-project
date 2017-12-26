e377c6952ee0d33b881f090020d74477e7ce36da
Oleksiy Alex <oleksiyalex3@gmail.com>
2017-12-26 19:24:12 +0200

    Make admin.html responsible

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function check(form) {
 	if(form.userid.value == "fwd-06-5" && form.passid.value == "qwert-12")
 	{
-		window.open('https://bu1der.github.io/about.html')
+		window.location = 'https://bu1der.github.io/about.html';
 	}
 	else
 	{
